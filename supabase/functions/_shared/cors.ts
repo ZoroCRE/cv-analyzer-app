1025d4b44643a1dd0a995f8e2f77c78308910c5d
Zoro_RCE <44169685+ZoroCRE@users.noreply.github.com>
2025-08-08 21:38:14 +0300

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,4 +1,4 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://cv-dshf.vercel.app', 
+  'Access-Control-Allow-Origin': 'https://cv-dshf.vercel.app', // Your frontend URL
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
