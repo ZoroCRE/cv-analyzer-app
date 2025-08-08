df53f6c3fc691e46e3dd67e5dc14da2200f0458e
Zoro_RCE <44169685+ZoroCRE@users.noreply.github.com>
2025-08-08 18:43:34 +0300

    Delete _shared directory

--- _shared/cors.ts
+++
@@ -1,4 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://cv-dshf.vercel.app', 
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-};
